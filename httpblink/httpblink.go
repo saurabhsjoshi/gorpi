4e0a2705e86b11e2af0e3764725e4c6e2e3ee5c5
Saurabh Joshi <battlex2010@yahoo.com>
2016-02-20 21:19:17 -0500

    Added message to notify of server start

--- httpblink/httpblink.go
+++ httpblink/httpblink.go
@@ -3,9 +3,11 @@ package httpblink
 import (
 	"log"
 	"net/http"
+	"fmt"
 )
 
 func StartServer(port string){
 	router := newRouter()
+	fmt.Println("Starting server on port " + port)
 	log.Fatal(http.ListenAndServe(":" + port, router))
 }
