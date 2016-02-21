e29c89b15cf40cafc137985e3010e681696d585a
Saurabh Joshi <battlex2010@yahoo.com>
2016-02-20 20:27:23 -0500

    Added missing parse

--- gorpi.go
+++ gorpi.go
@@ -11,7 +11,7 @@ func main(){
 	f_blink := flag.Bool("blink", false, "Toggles the LED")
 	f_pin := flag.Int("pin", -1, "Pin number")
 	f_pinState := flag.Int("state", -1, "Pin state 0 = OFF and 1 = ON")
-
+	flag.Parse()
 	if(*f_blink == true){
 		if(*f_pin != -1 && *f_pinState != -1){
 			if(*f_pinState != 0 || *f_pinState != 1){
